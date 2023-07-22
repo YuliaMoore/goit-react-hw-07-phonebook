c19f59dcab72ee60f5f68464ad2bab2c54d8474e
YuliaMoore <115689321+YuliaMoore@users.noreply.github.com>
2023-07-22 23:38:46 +0300

    12

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { List, Item, Button } from './ContactList.styled';
+import { useSelector, useDispatch } from 'react-redux';
+import { getVisibleContacts } from '../redux/selectors';
+import { removeContact } from '../redux/contacts-slice';
+
+// Компонент списка контактов
+const ContactList = () => {
+  const contacts = useSelector(getVisibleContacts);
+  const dispatch = useDispatch();
+  const handleDelete = () => dispatch(removeContact());
+  return (
+    <List>
+      {contacts.map(contact => (
+        <Item key={contact.id}>
+          {contact.name + ' : ' + contact.number}
+          {
+            <Button type="button" name="delete" onClick={handleDelete}>
+              delete
+            </Button>
+          }
+        </Item>
+      ))}
+    </List>
+  );
+};
+export default ContactList;
+
+ContactList.propTypes = {
+  contacts: PropTypes.object.isRequired,
+  onRemoveContact: PropTypes.func.isRequired,
+};
